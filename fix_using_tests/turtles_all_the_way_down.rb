04f83230f271cf05fb936d6b3efe616093b99478
Josh Allen <joshmrallen@gmail.com>
2020-03-04 16:43:55 +0000

    Automatically backed up by Learn

--- fix_using_tests/turtles_all_the_way_down.rb
+++ fix_using_tests/turtles_all_the_way_down.rb
@@ -3,7 +3,7 @@ require 'pry'
 def turtles
     [
       {name: "Donatello", weapon: "bo-staff", traits: ["serious", "leader"]},
-      {name: "Michaelangelo", weapon: "nunchuks", traits: "party dude"},
+      {name: "Michaelangelo", weapon: "nunchuks", traits: ["party dude"]},
       {name: "Raphael", weapon: "sais", traits: ["cool", "rude"]},
       {name: "Leonardo", weapon: "katanas", traits: ["loves being a turtle"]}
     ]
@@ -21,7 +21,7 @@ def turtle_traits(turtles)
     end
   end
 
-  traits[1] = ["party dude"]
+  # traits[1] = ["party dude"]
 
   return traits
   
