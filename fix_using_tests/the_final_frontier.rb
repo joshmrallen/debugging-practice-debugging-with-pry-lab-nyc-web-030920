a8cc729196b2adceff76ed15e30595af7c6c7cb4
Josh Allen <joshmrallen@gmail.com>
2020-03-04 16:10:42 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,4 +1,4 @@
-require 'pry'
+
 
 def generate_star_date
   (rand(100000) + 400000) / 10.0
@@ -15,8 +15,6 @@ end
 
 def greet_crew (crew)
   crew.map! {|crew_member| "Hello " + crew_member}
-  
-  binding.pry
 
 end
 
